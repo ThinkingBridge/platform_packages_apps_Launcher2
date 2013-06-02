169c5282e8459f73480f1f39e4f49a2b223129ea
htchoi1995 <htchoi1995@gmail.com>
2013-06-02 15:14:27 +0900

    Revert "Add support for statusbar/navbar transparency"
    This reverts commit b51bb62286af417c4e0eda40b134f5df6ada20b7.

--- src/com/android/launcher2/Launcher.java
+++ src/com/android/launcher2/Launcher.java
@@ -966,10 +966,6 @@ public final class Launcher extends Activity
         if (mSearchDropTargetBar != null) {
             mSearchDropTargetBar.setup(this, dragController);
         }
-        
-        if(Settings.System.getFloat(getContentResolver(), "navigation_bar_alpha", 1.0f) < 1) {
-        	mDragLayer.setBackground(null);
-        }
     }
 
     /**
